4a751c045e94ff383aa1445aa34249fba4a8a535
liuyao <liuyao@ejoy.com>
2019-12-12 19:52:12 +0800

    修改Upsert方法

--- src/main/java/com/fly/mybatis/phoenix/Upsert.java
+++ src/main/java/com/fly/mybatis/phoenix/Upsert.java
@@ -2,7 +2,10 @@ package com.fly.mybatis.phoenix;
 
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.TableInfoHelper;
 import com.baomidou.mybatisplus.core.toolkit.sql.SqlScriptUtils;
+import org.apache.ibatis.executor.keygen.KeyGenerator;
 import org.apache.ibatis.executor.keygen.NoKeyGenerator;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
@@ -24,16 +27,30 @@ public class Upsert extends AbstractMethod {
     @Override
     public MappedStatement injectMappedStatement(Class<?> mapperClass, Class<?> modelClass, TableInfo tableInfo) {
         PhoenixSqlMethod sqlMethod = PhoenixSqlMethod.UPSERT_ONE;
+        KeyGenerator keyGenerator = new NoKeyGenerator();
         String columnScript = SqlScriptUtils.convertTrim(tableInfo.getAllInsertSqlColumnMaybeIf(),
                 LEFT_BRACKET, RIGHT_BRACKET, null, COMMA);
         String valuesScript = SqlScriptUtils.convertTrim(tableInfo.getAllInsertSqlPropertyMaybeIf(null),
                 LEFT_BRACKET, RIGHT_BRACKET, null, COMMA);
 
+        String keyProperty = null;
+        String keyColumn = null;
+
+        if (StringUtils.isNotEmpty(tableInfo.getKeyProperty())) {
+            // 自增主键会造成HBase单Region数据挤压，直接移除
+            if (null != tableInfo.getKeySequence()) {
+                keyGenerator = TableInfoHelper.genKeyGenerator(tableInfo, builderAssistant, sqlMethod.getMethod(), languageDriver);
+                keyProperty = tableInfo.getKeyProperty();
+                keyColumn = tableInfo.getKeyColumn();
+            }
+        }
+
+
         String sql = String.format(sqlMethod.getSql(), tableInfo.getTableName(), columnScript, valuesScript);
         SqlSource sqlSource = languageDriver.createSqlSource(configuration, sql, modelClass);
 
-        // TODO 主键生成和序列不兼容
-        return this.addInsertMappedStatement(mapperClass, modelClass, sqlMethod.getMethod(), sqlSource, new NoKeyGenerator(), null, null);
+        return this.addInsertMappedStatement(mapperClass, modelClass, sqlMethod.getMethod(), sqlSource, keyGenerator, keyProperty, keyColumn);
+
     }
 
 }
