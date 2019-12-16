f8489608a740960a2a50471da0d794d6dc7ce3a3
liuyao <liuyao@ejoy.com>
2019-12-16 15:47:50 +0800

    1. 升级Springboot到2.1.5到2.2.0

--- src/main/java/com/github/flyingglass/mybatis/boot/FgMybatisAutoConfiguration.java
+++ src/main/java/com/github/flyingglass/mybatis/boot/FgMybatisAutoConfiguration.java
@@ -2,14 +2,12 @@ package com.github.flyingglass.mybatis.boot;
 
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.SqlExplainInterceptor;
 import com.github.flyingglass.mybatis.cache.ApplicationContextHolder;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
 
 /**
  * @author fly
@@ -25,12 +23,5 @@ public class FgMybatisAutoConfiguration {
     @ConditionalOnMissingBean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    @Profile({"dev", "default"})
-    public SqlExplainInterceptor sqlExplainInterceptor() {
-        return new SqlExplainInterceptor();
     }
 }
