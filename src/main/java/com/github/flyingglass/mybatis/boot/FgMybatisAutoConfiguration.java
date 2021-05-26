e61e8c932fbdf22affdef787a09ed12aee34020d
fly <yao.liu@tvbc.com.cn>
2021-05-26 14:41:15 +0800

    添加conditional依赖

--- src/main/java/com/github/flyingglass/mybatis/boot/FgMybatisAutoConfiguration.java
+++ src/main/java/com/github/flyingglass/mybatis/boot/FgMybatisAutoConfiguration.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.github.flyingglass.mybatis.cache.ApplicationContextHolder;
 import com.github.flyingglass.mybatis.config.DefaultMetaObjectHandler;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,13 +23,14 @@ public class FgMybatisAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnClass(PaginationInterceptor.class)
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
 
-
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnClass(MetaObjectHandler.class)
     public MetaObjectHandler metaObjectHandler() {
         return new DefaultMetaObjectHandler();
     }
